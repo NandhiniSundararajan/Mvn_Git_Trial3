d9f922f5de884fb4549ca009ebd4d3bfe816efe5
NandhiniSundararajan <nandysundar@gmail.com>
2023-05-12 12:52:59 -0400

    first commit

---
+++ src/test/java/NewTests/Checks.java
@@ -0,0 +1,21 @@
+package NewTests;
+
+import org.testng.annotations.Test;
+
+public class Checks {
+
+    @Test
+    public void test1(){
+        System.out.println("this comes from 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("this comes from 2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("this comes from 3");
+    }
+}
